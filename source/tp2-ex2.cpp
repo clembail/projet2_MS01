d084da369049a77643ec8a92f99bcd0d23a0f3fe
Clément Baillet <c.bailletpro@icloud.com>
2025-11-12 22:38:22 +0100

    codes sources projet

--- source/tp2-ex2.cpp
+++ source/tp2-ex2.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-#include <femtool>
+#include "femtool.hpp"
 
 
 int main(int argc,char* argv[]){
-    std::cout << "Hello World !" << std::endl;
+
+    std::string input = "./solutions/mesh_h_0.01.mesh";
+    std::string output = "./solutions/mesh_h_0.01_partition4.mesh";
+
+    Mesh2D Omega;
+    Read(Omega, input);
+
+    std::cout << "Partitionnement du maillage" << std::endl;
+    auto [Sigma, Q] = Partition4(Omega);
+
+    std::cout << "Création du maillage prenant en compte le partionnement" << std::endl;
+    Plot(Sigma, output);
+
     return 0;
 }
