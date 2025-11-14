0be91b31c47e4fcc9b8862cb3e16e0c5afa2b8ae
Clément Baillet <c.bailletpro@icloud.com>
2025-11-14 19:15:34 +0100

    bibliothèque femtool avec partition.hpp

--- femtool/partition.hpp
+++ femtool/partition.hpp
@@ -5,6 +5,7 @@
 #include "element.hpp"
 #include "mesh.hpp"
 #include <cstddef>
+#include <tuple>
 
 using Mesh2DPart = std::vector<Mesh2D>;
 
@@ -20,14 +21,18 @@ Partition4(const Mesh2D& Omega){
     CooMatrix<double> Q(nb_elements,4);
 
     // calcul des coordonnées min/max et du centre
-    double minX = 0 ; double maxX = 0 ;
-    double minY = 0 ; double maxY = 0 ;
+    double minX = 1.e10 ; double maxX = -1.e10 ;
+    double minY = 1.e10 ; double maxY = -1.e10 ;
 
-    for (const auto& node : nodes){
-        if(node[0] < minX) minX = node[0];
-        if(node[0] > maxX) maxX = node[0];
-        if(node[1] < minY) minY = node[1];
-        if(node[1] > maxY) maxY = node[1];
+    for(const auto& elt : Omega) {
+        for(std::size_t k=0; k<3; ++k) {
+            const auto& node = elt[k];
+            
+            if(node[0] < minX) minX = node[0];
+            if(node[0] > maxX) maxX = node[0];
+            if(node[1] < minY) minY = node[1];
+            if(node[1] > maxY) maxY = node[1];
+        }
     }
 
     double centreX = (minX + maxX)/2;
@@ -53,11 +58,86 @@ Partition4(const Mesh2D& Omega){
     return {Sigma, Q};
 };
 
+void Plot(const std::vector<Mesh2D>& Sigma,
+    std::filesystem::path    filename){
+        
+    if (Sigma.empty()) return;
+
+    std::vector<std::string> tag =
+    {"Vertices", "Edges", "Triangles", "Tetrahedra"};
+    
+    // Écriture du fichier .mesh pour vizir4 et gmsh
+    filename.replace_extension(".mesh");
+    std::ofstream f;
+    f.open(filename.c_str());
+
+    f << "MeshVersionFormatted 3\n\n";
+    f << "Dimension\n3\n\n";
+    
+    int nb_domains = Sigma.size();
+    std::size_t nb_nodes = (Sigma[0].nodes()).size();
+    std::size_t nb_elements = 0;
+    std::size_t DIM = 2;
+
+    auto v = Sigma[0].nodes();
+    auto& v0 = v[0];
+
+    for (std::size_t i = 0; i < nb_domains; i++){
+        std::cout << Sigma[i].size() << std::endl;
+        nb_elements += Sigma[i].size();
+    }
+
+    f << "Vertices\n";
+    f << nb_nodes << "\n";
+    for(const auto& x:v){
+        f << x << "\t1\n";
+    }
+    f << "\n";
+
+    f << tag[DIM] << "\n";
+    f << nb_elements << "\n";
+    for (std::size_t j = 0; j < nb_domains; j++){
+        auto m = Sigma[j];
+        for(const auto& e:m){
+            for(std::size_t k=0; k<DIM+1; k++){
+                f << 1 + int(&e[k]-&v0) << "\t";
+            }
+            f << (j+1) << "\n";
+        }
+    }
+
+    f << "\nEnd";
+    f.close();
+
+    // Écriture du fichier .sol pour vizir4
+    filename.replace_extension(".sol");
+    std::ofstream f_sol;
+    f_sol.open(filename.c_str());
+
+    f_sol << "MeshVersionFormatted 3\n\nDimension\n3\n\n";
+
+    f_sol << "SolAtTriangles\n";
+    f_sol << nb_elements << "\n";
+    f_sol << "1 1\n";
+
+    for (std::size_t j = 0; j < Sigma.size(); ++j) {
+        double partition_value = (double)(j + 1);
+        
+        for (std::size_t i = 0; i < Sigma[j].size(); ++i) {
+            f_sol << partition_value << "\n";
+        }
+    }
+
+    f_sol << "\nEnd";
+    f_sol.close();
+  
+}
+
 // Surcharge de la fonction Partition4
 std::pair<std::vector<Mesh2D>, CooMatrix<double>> 
 Partition4(const Mesh2D& Omega, const std::size_t& nl) {
 
-    // 1. Récupérer la partition de base (sans recouvrement)
+    // Récupérer la partition de base (sans recouvrement)
     auto [Sigma, Q] = Partition4(Omega);
 
     std::vector<Mesh2D> Gamma;
@@ -127,64 +207,118 @@ Partition4(const Mesh2D& Omega, const std::size_t& nl) {
     return {Gamma, R};
 }
 
-void Plot(const std::vector<Mesh2D>& Sigma,
-    std::filesystem::path    filename){
-        
-    if (Sigma.empty()) return;
+std::pair< Mesh2DPart,CooMatrix<double> > 
+Partition16(const Mesh2D& Omega){
+    auto [Sigma,Q] = Partition4(Omega);
+    std::size_t nb_elements = Omega.size();
 
-    std::vector<std::string> tag =
-    {"Vertices", "Edges", "Triangles", "Tetrahedra"};
+    Mesh2DPart Sigma_16;
+    Sigma_16.reserve(16);
 
-    // Ouverture fichier
-    filename.replace_extension(".mesh");
-    std::ofstream f;
-    f.open(filename.c_str());
+    CooMatrix<double> Q_16(nb_elements,16);
+    Q_16.reserve(Omega.size());
 
-    // Préambule
-    f << "MeshVersionFormatted 3\n\n";
-    f << "Dimension\n3\n\n";
-    
-    int nb_domains = Sigma.size();
-    std::size_t nb_nodes = (Sigma[0].nodes()).size();
-    std::size_t nb_elements = 0;
-    std::size_t DIM = 2;
-
-    auto v = Sigma[0].nodes();
-    auto& v0 = v[0];
-
-    for (std::size_t i = 0; i < nb_domains; i++){
-        // nb_nodes += (Sigma[i].nodes()).size();
-        std::cout << Sigma[i].size() << std::endl;
-        nb_elements += Sigma[i].size();
-    }
-
-    // Section noeuds
-    // auto v = m.nodes();
-    // const auto& v0 = v[0];
-    f << "Vertices\n";
-    f << nb_nodes << "\n";
-    for(const auto& x:v){
-        f << x << "\t1\n";
-    }
-    f << "\n";
-
-    // Section elements
-    f << tag[DIM] << "\n";
-    f << nb_elements << "\n";
-    for (std::size_t j = 0; j < nb_domains; j++){
-        auto m = Sigma[j];
-        for(const auto& e:m){
-            for(std::size_t k=0; k<DIM+1; k++){
-                f << 1 + int(&e[k]-&v0) << "\t";
-            }
-            f << (j+1) << "\n";
+    std::vector<std::vector<std::size_t>> global_indices(4);
+    for(const auto& [row, col, val] : GetData(Q)) {
+        if (val > 0.0) {
+            global_indices[col].push_back(row);
         }
     }
 
-    // Fermeture
-    f << "\nEnd";
-    f.close();
-  
+    for (std::size_t p = 0; p < 4; p++){
+        auto [Sigma_local,Q_local] = Partition4(Sigma[p]);
+        Sigma_16.insert(Sigma_16.end(), Sigma_local.begin(), Sigma_local.end());
+
+        const auto& map_p = global_indices[p]; // Le dictionnaire pour p
+
+        for(const auto& [local_row, local_col, val] : GetData(Q_local)) {
+            if (val > 0.0) {
+                std::size_t global_row = map_p[local_row];
+                
+                std::size_t final_col = local_col + (p * 4);
+
+                Q_16.push_back(global_row, final_col, 1.0);
+            }
+        }
+    }
+
+    Q_16.sort();
+
+    return {Sigma_16, Q_16};
+}
+
+// Surcharge de la fonction Partition16
+std::pair<std::vector<Mesh2D>, CooMatrix<double>> 
+Partition16(const Mesh2D& Omega, const std::size_t& nl) {
+
+    // Récupérer la partition de base (sans recouvrement)
+    auto [Sigma, Q] = Partition16(Omega);
+
+    std::vector<Mesh2D> Gamma;
+    Gamma.reserve(16);
+    CooMatrix<double> R(Omega.size(), 16);
+
+    // Pré-traitement de Q pour récupérer les listes d'éléments initiaux
+    std::vector<std::vector<std::size_t>> elements_in_p(16);
+    
+    for(const auto& [row, col, val] : GetData(Q)) {
+        if (val > 0.0) {
+            elements_in_p[col].push_back(row);
+        }
+    }
+
+    // Boucle principale sur les 4 sous-domaines
+    for(std::size_t p = 0; p < 16; ++p) {
+        
+        // Initialisation avec les données de Q
+        std::vector<std::size_t> current_elements = elements_in_p[p];
+
+        std::vector<bool> is_in_gamma(Omega.size(), false);
+        for(auto idx : current_elements) {
+            is_in_gamma[idx] = true;
+        }
+
+        // Ajout des couches (Overlap)
+        auto nodes = Omega.nodes();
+        
+        for(std::size_t layer = 0; layer < nl; ++layer) {
+            std::vector<bool> active_nodes(nodes.size(), false);
+            for(auto elt_idx : current_elements) {
+                const auto& e = Omega[elt_idx];
+                active_nodes[int(&e[0] - &nodes[0])] = true;
+                active_nodes[int(&e[1] - &nodes[0])] = true;
+                active_nodes[int(&e[2] - &nodes[0])] = true;
+            }
+
+            for(std::size_t k = 0; k < Omega.size(); ++k) {
+                if(is_in_gamma[k]) continue; 
+
+                const auto& e = Omega[k];
+                if( active_nodes[int(&e[0] - &nodes[0])] || 
+                    active_nodes[int(&e[1] - &nodes[0])] || 
+                    active_nodes[int(&e[2] - &nodes[0])] ) {
+                    
+                    is_in_gamma[k] = true;
+                    current_elements.push_back(k);
+                }
+            }
+        }
+
+        // Construction de Gamma[p] et remplissage de R
+        Mesh2D mesh_p(nodes);
+        
+        for(auto global_idx : current_elements) {
+            mesh_p.push_back(Omega[global_idx]);
+            
+            double local_idx = (double)(mesh_p.size() - 1);
+            R.push_back(global_idx, p, local_idx);
+        }
+        
+        Gamma.push_back(mesh_p);
+    }
+    
+    R.sort();
+    return {Gamma, R};
 }
 
 #endif
